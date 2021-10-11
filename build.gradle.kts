945d3ab3e1d9ea6c636a4695707ad3d4c3b1f3a0
Seop(Home Mac) <edenhazard5870@gmail.com>
2021-10-11 18:38:01 +0900

    setting - intellij platform plugin delete

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,16 +12,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    version.set("2021.2.1")
-}
 tasks {
-    patchPluginXml {
-        changeNotes.set("""
-            Add change notes here.<br>
-            <em>most HTML tags may be used</em>        """.trimIndent())
-    }
+
 }
