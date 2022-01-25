9e0ca1a93beab3d33fd07b390e2b5e7854b07500
just-hazard <edenhazard5870@gmail.com>
2022-01-25 20:15:32 +0900

    feat, test - scope function example

---
+++ src/test/kotlin/referance/ScopeFunction.kt
@@ -0,0 +1,29 @@
+package referance
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ScopeFunction {
+
+    @Test
+    fun `let Test`() {
+        Person("이름",25).let {
+            assertThat(it.name).isEqualTo("이름")
+            assertThat(it.age).isEqualTo(25)
+        }
+    }
+
+    @Test
+    fun `with Test`() {
+        with(Person("이름", 25)) {
+            assertThat(this.name).isEqualTo("이름")
+            assertThat(this.age).isEqualTo(25)
+        }
+    }
+}
+
+data class Person(
+    val name: String,
+    val age: Int
+)
+
